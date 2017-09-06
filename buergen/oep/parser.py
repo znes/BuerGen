9f48cd4efe44bd44e20cb6e45d2465e7a47dc409
Martin <martin.soethe@uni-flensburg.de>
2017-09-06 17:09:35 +0200

    Add OepParser class
    Containing methods for calling the oep-api and checking http response
    codes.

---
+++ buergen/oep/parser.py
@@ -0,0 +1,56 @@
+"""
+"""
+
+from .io import request_and_response
+
+
+class OepParser(object):
+    """ Call the OEP """
+
+    def __init__(self, *args, **kwargs):
+
+        self.schema = kwargs.get('schema')
+        self.table = kwargs.get('table')
+        self.token = kwargs.get('token')
+        self.apiurl = kwargs.get('apiurl')
+        self.taburl = self.apiurl + 'schema' + '/' + self.schema +\
+            '/' + 'tables' + '/' + self.table + '/'
+
+    def check_table_exists(self):
+
+        try:
+            request_and_response(self.taburl, 'get', 200)
+            print("Table does exist.")
+            return True
+
+        except AssertionError:
+            print("Table does not exist yet.")
+
+    def create_table(self, body):
+
+        try:
+            request_and_response(self.taburl, 'put', 201, body=body,
+                                 token=self.token)
+
+        except AssertionError:
+            print("Oops, table could not be created.")
+
+    def insert_into_table(self, body, index=None):
+
+        call = "rows/" + str(index + 1) if index else "rows/new"
+
+        try:
+
+            request_and_response(self.taburl + call, 'put', 201,
+                                 body=body, token=self.token)
+
+        except AssertionError:
+            print("Oops, could not insert data.".format(index))
+
+    def delete_table(self):
+
+        try:
+            request_and_response(self.taburl, 'del', 200, token=self.token)
+
+        except AssertionError:
+            print("Oops, could not delete table.")
