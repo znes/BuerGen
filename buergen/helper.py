cd0ef26e171ca17e8397d594b758a70c490ced21
Martin <martin.soethe@uni-flensburg.de>
2017-09-21 17:43:05 +0200

    Update docstrings

--- buergen/helper.py
+++ buergen/helper.py
@@ -1,10 +1,21 @@
 from sys import stdout
 
+
 def yes_or_no(msg):
-    """
+    """ Simple yes or no question asks for user input
+
+    Attributes
+    ----------
+    msg : str
+        Question to be asked.
+
+    Returns
+    -------
+    bool
+        True if positive false otherwise.
     """
 
-    valid = {"yes": True, "y":True, "no": False, "n":False}
+    valid = {"yes": True, "y": True, "no": False, "n": False}
     prompt = " [y/n] "
 
     while True:
@@ -14,5 +25,3 @@ def yes_or_no(msg):
             return valid[choice]
         else:
             stdout.write("Please respond with 'y' or 'n'.")
-
-
